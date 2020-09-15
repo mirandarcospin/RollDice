ca8bca1c22ee920d1413778011366dad2cc3cdf7
mirandarcospin <mxr17e@acu.edu>
2020-09-14 20:36:11 -0500

    Sounds and 7-20 dice (MRC_9/14/20)

--- app/src/main/java/com/example/rolldice/MainActivity.java
+++ app/src/main/java/com/example/rolldice/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.rolldice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -12,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     /*Private variables, one for the dice image and the other
         one for the random variable (gives the random number) */
     private ImageView imageViewDice;
+    private TextView textViewDice;
     private Random rng = new Random();
+    /*Sounds*/
+    private MediaPlayer rolling;
+    private MediaPlayer diceMiss;
+    private MediaPlayer diceHit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewDice = findViewById(R.id.textView); /* For text. Starts with "Roll!!!" */
+        textViewDice.setText(R.string.start_text);
 
         imageViewDice = findViewById(R.id.image_view_dice);
         imageViewDice.setOnClickListener(new View.OnClickListener() {
@@ -31,11 +42,22 @@ public class MainActivity extends AppCompatActivity {
     private void rollDice()
     { /*Made the random function working. Difference cases to show
         the different dice images */
-        int randomNumber = rng.nextInt(6) + 1;
+        textViewDice.setText(""); /*So there is no text during the rolls just the
+                                    critical texts when dice is 1 or 20*/
+        /*Setting the sounds*/
+        rolling = MediaPlayer.create(MainActivity.this, R.raw.rolldice);
+        diceMiss = MediaPlayer.create(MainActivity.this, R.raw.miss);
+        diceHit = MediaPlayer.create(MainActivity.this, R.raw.hit);
+
+        rolling.start();/*When rolling any dice*/
+
+        int randomNumber = rng.nextInt(20) + 1;
         switch(randomNumber)
         {
-            case 1:
+            case 1: /*Text and Sound*/
                 imageViewDice.setImageResource(R.drawable.dice1);
+                textViewDice.setText(R.string.cm_d1);
+                diceMiss.start();
                 break;
             case 2:
                 imageViewDice.setImageResource(R.drawable.dice2);
@@ -52,6 +74,52 @@ public class MainActivity extends AppCompatActivity {
             case 6:
                 imageViewDice.setImageResource(R.drawable.dice6);
                 break;
+            case 7:
+                imageViewDice.setImageResource(R.drawable.dice7);
+                break;
+            case 8:
+                imageViewDice.setImageResource(R.drawable.dice8);
+                break;
+            case 9:
+                imageViewDice.setImageResource(R.drawable.dice9);
+                break;
+            case 10:
+                imageViewDice.setImageResource(R.drawable.dice10);
+                break;
+            case 11:
+                imageViewDice.setImageResource(R.drawable.dice11);
+                break;
+            case 12:
+                imageViewDice.setImageResource(R.drawable.dice12);
+                break;
+            case 13:
+                imageViewDice.setImageResource(R.drawable.dice13);
+                break;
+            case 14:
+                imageViewDice.setImageResource(R.drawable.dice14);
+                break;
+            case 15:
+                imageViewDice.setImageResource(R.drawable.dice15);
+                break;
+            case 16:
+                imageViewDice.setImageResource(R.drawable.dice16);
+                break;
+            case 17:
+                imageViewDice.setImageResource(R.drawable.dice17);
+                break;
+            case 18:
+                imageViewDice.setImageResource(R.drawable.dice18);
+                break;
+            case 19:
+                imageViewDice.setImageResource(R.drawable.dice19);
+                break;
+            case 20:/*Text and sound*/
+                imageViewDice.setImageResource(R.drawable.dice20);
+                textViewDice.setText(R.string.ch_d20);
+                diceHit.start();
+                break;
         }
+
     }
+
 }
